80b245b9b5d3ef06f999eb535955cc76aad8b100
myakayev <myakayev@yahoo.com>
2022-02-02 22:53:33 +0200

    First commit

--- lesson1-app/src/main/java/school/lesson1/MainApp.java
+++ lesson1-app/src/main/java/school/lesson1/MainApp.java
@@ -2,6 +2,6 @@ package school.lesson1;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Срака в жопе");
+        System.out.println("приличный текст");
     }
 }
